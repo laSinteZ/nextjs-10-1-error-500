f7e753d4627b292aefd6a2dacaa91eefabcfffdd
Vyacheslav Bikbaev <la.sintez@gmail.com>
2021-03-31 10:55:12 +0300

    Repro for 500 build bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import App from "next/app";
 import '../styles/globals.css'
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  // Build error happens because error page tries to fetch this on build time when backend isn't availagble
+  await fetch('http://127.0.0.1/some-backend-url');
+  return { ...appProps }
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
